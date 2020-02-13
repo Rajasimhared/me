41df2b5eaef1599ac6fd0a6f2dba885a8e0569d6
Rajasimha Reddy <rajasimha5599@gmail.com>
2020-02-13 19:53:14 +0530

    css doc

--- src/components/Dock/index.js
+++ src/components/Dock/index.js
@@ -3,15 +3,17 @@ import "./style.scss";
 
 const Dock = ({ data }) => (
   <div id="dock-container">
-    <div class="circle-container">
-      {data.map(value => (
-        <li>
-          <span>{value.name}</span>
-          <a href="#">
-            <img src={value.imageURL} />
-          </a>
-        </li>
-      ))}
+    <div id="dock">
+      <ul>
+        {data.map(value => (
+          <li>
+            <span>{value.name}</span>
+            <a href="#">
+              <img src={value.imageURL} />
+            </a>
+          </li>
+        ))}
+      </ul>
     </div>
   </div>
 );
