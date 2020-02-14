0a35988203c0d600d4d4f3b4403cd6e8ddb4b94b
Rajasimha Reddy <rajasimha5599@gmail.com>
2020-02-14 19:17:03 +0530

    fix image commit

--- src/App.js
+++ src/App.js
@@ -2,13 +2,17 @@ import React from "react";
 import { DockData } from "./utils/Constants";
 import Dock from "./components/Dock";
 import DetailsBar from "./components/DetailsBar";
+import Me from "./RR.JPG";
 import "./App.css";
 
 function App() {
   return (
     <>
       <div className="App">
-        <div className="portfolio-image"></div>
+        <div
+          className="portfolio-image"
+          style={{ backgroundImage: `url(${Me})` }}
+        ></div>
         <DetailsBar />
       </div>
       <Dock data={DockData} />
