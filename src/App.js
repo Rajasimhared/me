e3fe6834d81c3d86c601630cc561fbf9a706d509
Rajasimha Reddy <rajasimha5599@gmail.com>
2020-02-14 18:38:56 +0530

    Give life to page

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
-import React from 'react';
-import { DockData } from './utils/Constants'
-import Dock from './components/Dock';
-import './App.css';
+import React from "react";
+import { DockData } from "./utils/Constants";
+import Dock from "./components/Dock";
+import DetailsBar from "./components/DetailsBar";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      Work in progress...
-      <Dock data={DockData}/>
-    </div>
+    <>
+      <div className="App">
+        <div className="portfolio-image"></div>
+        <DetailsBar />
+      </div>
+      <Dock data={DockData} />
+    </>
   );
 }
 
